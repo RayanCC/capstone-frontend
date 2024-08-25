81d96d0026ae40383a04c11880bc420acff79a4e
Rayan <rayan5ye@hotmail.com>
2024-08-24 21:19:01 -0400

    adding footer contents,links

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,13 @@
 import "../Footer/footer.scss";
 import BackToTopButton from "../BackToTopButton/BackToTopButton";
+import FooterNav from "../FooterNav/FooterNav";
 
 
 const Footer = () => {
     return (
       <div className="footer">
         <BackToTopButton />
+        <FooterNav />
         <p className="footer-copyright">&copy;BlueFamily Inc. All Right Reserved. </p>
       </div>
     );
---
+++ src/components/FooterNav/FooterNav.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import FooterNavCol from '../FooterNavCol/FooterNavCol'
+
+const FooterNav = () => {
+  return (
+    <div className="footernavcol-container">
+      <FooterNavCol />
+    </div>
+  )
+}
+
+export default FooterNav
