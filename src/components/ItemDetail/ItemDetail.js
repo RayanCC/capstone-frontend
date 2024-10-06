c5567c473e8deb69a9240a66f580a5f0509e963a
Rayan <rayan5ye@hotmail.com>
2024-10-06 16:37:43 -0400

    adding util. constant for images exporting to banner

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import { images } from "../Banner/Banner";
+import { images } from "../../util/Constant";
 import "../ItemDetail/itemdetail.scss";
 
 const ItemDetail = () => {
